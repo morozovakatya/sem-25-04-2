9e54e8ce3c002a1cfe9b0d1d0bfb5e9ccbe9555e
katya <hellifire@mail.ru>
2022-04-25 20:56:38 +0300

    square num proj

---
+++ task001/Program.cs
@@ -0,0 +1,8 @@
+﻿//программа, в которую вводишь число и она выдает его квадрат
+
+Console.Write("Введите целое число  ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен  ");
+Console.WriteLine(numResult);
+Console.ReadKey();
