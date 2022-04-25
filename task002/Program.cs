4056534d686e5026dd0f4d762fe4e0573222485e
katya <hellifire@mail.ru>
2022-04-25 21:41:05 +0300

    002

---
+++ task002/Program.cs
@@ -0,0 +1,9 @@
+﻿//программа, в которую вводишь 2 числа и она проверяет, является ли оно квадратом второго числа
+
+Console.Write("Введите первое число  ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число  ");
+int numB = int.Parse(Console.ReadLine());
+bool result = numB * numB == numA;
+Console.WriteLine(result ? "Да" : "Нет");
+Console.ReadKey(); 
