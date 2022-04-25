2e3612eb2dc523d7dbfef0e7f60de34ad2cf3700
katya <hellifire@mail.ru>
2022-04-25 22:29:39 +0300

    added comment

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,4 +1,4 @@
 ﻿// трехзначное число и на выходе показыввет последнюю цифру числа
 
 int num = int.Parse(Console.ReadLine());
-Console.WriteLine(num % 10);
+Console.WriteLine(num % 10); //остаток от деления на любое число
